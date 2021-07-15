da752fbcacb7032de41e6c2887ba64c882efc748
Benjamin Akar <benjaminakar2001@gmail.com>
2021-07-15 10:59:11 +0200

    Fix visual styling after a11y fixes to radio-buttons

--- packages/toggle/src/component.tsx
+++ packages/toggle/src/component.tsx
@@ -4,6 +4,30 @@ import { ToggleEntry, ToggleProps } from './props';
 import { classNames } from '@chbphone55/classnames';
 import { Item } from './item';
 
+function Title({ id, isInvalid, title }) {
+    return (
+        <legend
+            id={`${id}__title`}
+            className={classNames('field-label', {
+                'text-danger': isInvalid,
+            })}
+        >
+            {title}
+        </legend>
+    );
+}
+
+function HelpText({ isInvalid, helpId, helpText }: any) {
+    return (
+        <div
+            id={helpId}
+            className={classNames('field-hint', { 'text-danger': isInvalid })}
+        >
+            {helpText}
+        </div>
+    );
+}
+
 export function Toggle(props: ToggleProps) {
     const id = useId();
 
@@ -13,68 +37,74 @@ export function Toggle(props: ToggleProps) {
         props.type === 'radio' || props.type === 'radio-button';
 
     return (
-        <fieldset
-            role={isRadioGroup ? 'radiogroup' : undefined}
-            aria-invalid={isRadioGroup ? isInvalid : undefined}
-            aria-errormessage={isRadioGroup && isInvalid ? helpId : undefined}
-            aria-describedby={isRadioGroup && !isInvalid ? helpId : undefined}
-            className={classNames({
-                'segment-control': props.type === 'radio-button',
-                'segment-control--justified': props.equalWidth,
-                'segment-control--small': props.small,
-                'input-toggle':
-                    props.type === 'radio' || props.type === 'checkbox',
-            })}
-        >
-            {props.title && (
-                <legend
-                    id={`${id}__title`}
-                    className={classNames('field-label', {
-                        'text-danger': isInvalid,
-                    })}
-                >
-                    {props.title}
-                </legend>
+        <>
+            {props.type === 'radio-button' && props.title && (
+                <Title id={id} title={props.title} isInvalid={isInvalid} />
             )}
-            {!props.options && props.label ? (
-                <Item
-                    label={props.label}
-                    checked={props.checked}
-                    onChange={(e: boolean) => props.onChange(e)}
-                    name={`${id}:toggle`}
-                    key={`${id + props.type}`}
-                    invalid={isInvalid}
-                    helpId={helpId}
-                    type={isRadioGroup ? 'radio' : 'checkbox'}
-                />
-            ) : (
-                props.options &&
-                props.options.map((option, i) => (
+            <fieldset
+                role={isRadioGroup ? 'radiogroup' : undefined}
+                aria-invalid={isRadioGroup ? isInvalid : undefined}
+                aria-errormessage={
+                    isRadioGroup && isInvalid ? helpId : undefined
+                }
+                aria-describedby={
+                    isRadioGroup && !isInvalid ? helpId : undefined
+                }
+                className={classNames({
+                    'segment-control': props.type === 'radio-button',
+                    'segment-control--justified': props.equalWidth,
+                    'segment-control--small': props.small,
+                    'input-toggle':
+                        props.type === 'radio' || props.type === 'checkbox',
+                })}
+            >
+                {props.type !== 'radio-button' && props.title && (
+                    <Title id={id} title={props.title} isInvalid={isInvalid} />
+                )}
+                {!props.options && props.label ? (
                     <Item
-                        checked={props.selected?.some(
-                            (s) => s.value === option.value,
-                        )}
-                        option={option}
-                        onChange={(e: ToggleEntry) => props.onChange(e)}
+                        label={props.label}
+                        checked={props.checked}
+                        onChange={(e: boolean) => props.onChange(e)}
                         name={`${id}:toggle`}
-                        key={`${id + i + props.type}`}
+                        key={`${id + props.type}`}
                         invalid={isInvalid}
                         helpId={helpId}
                         type={isRadioGroup ? 'radio' : 'checkbox'}
                     />
-                ))
-            )}
+                ) : (
+                    props.options &&
+                    props.options.map((option, i) => (
+                        <Item
+                            checked={props.selected?.some(
+                                (s) => s.value === option.value,
+                            )}
+                            option={option}
+                            onChange={(e: ToggleEntry) => props.onChange(e)}
+                            name={`${id}:toggle`}
+                            key={`${id + i + props.type}`}
+                            invalid={isInvalid}
+                            helpId={helpId}
+                            type={isRadioGroup ? 'radio' : 'checkbox'}
+                        />
+                    ))
+                )}
 
-            {props.helpText && (
-                <div
-                    id={helpId}
-                    className={classNames('field-hint', {
-                        'text-danger': isInvalid,
-                    })}
-                >
-                    {props.helpText}
-                </div>
+                {props.type !== 'radio-button' && props.helpText && (
+                    <HelpText
+                        helpId={helpId}
+                        helpText={props.helpText}
+                        isInvalid={isInvalid}
+                    />
+                )}
+            </fieldset>
+            {props.type === 'radio-button' && props.helpText && (
+                <HelpText
+                    helpId={helpId}
+                    helpText={props.helpText}
+                    isInvalid={isInvalid}
+                />
             )}
-        </fieldset>
+        </>
     );
 }
