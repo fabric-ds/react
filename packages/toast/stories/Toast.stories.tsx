3d9da5505e4295ad843cef0131f2f37f1e68d4f3
Benjamin Akar <benjaminakar2001@gmail.com>
2021-07-20 15:29:46 +0200

    removed breaking stories

--- packages/toast/stories/Toast.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { ToastContainer } from '../src';
-
-const metadata = { title: 'Layout/Toast' };
-export default metadata;
-
-export const Example = () => {
-    return <ToastContainer />;
-};
