efe60c52812d3d5466baf432396f1c6532d87112
Benjamin Akar <benjaminakar2001@gmail.com>
2021-07-26 16:18:27 +0200

    Update component classes

--- packages/modal/stories/Modal.stories.tsx
+++ packages/modal/stories/Modal.stories.tsx
@@ -1,84 +1,71 @@
 import * as React from 'react';
-import { Modal, ModalContent, ModalFooter, ModalHeading } from '../src';
+import { Modal } from '../src';
 import { Button } from '../../button/src';
 
 const metadata = { title: 'Overlays/Modal' };
 export default metadata;
 
 export const Example = () => {
-    const [isOpen, setIsOpen] = React.useState(true);
-    const ctaRef = React.useRef(null);
-    const handleOpen = () => setIsOpen(true);
-
-    const handleClose = () => setIsOpen(false);
+    const [open, setOpen] = React.useState(true);
+    const toggleModal = () => setOpen(!open);
 
     return (
         <>
-            <Button onClick={handleOpen}>Open modal</Button>
-            <Modal isOpen={isOpen} onDismiss={handleClose}>
-                <ModalContent>
-                    <div className="u-bg-toothpaste rounded-8 pt-64 pb-32 mb-8" />
-                    <ModalHeading className="mt-16">
-                        Title of the content goes here
-                    </ModalHeading>
-                    <p>
-                        Content information goes here. Optional illustration on
-                        top. Can contain links.
-                    </p>
-                    <a href="#" onClick={(event) => event.preventDefault()}>
-                        Optional link to read more.
-                    </a>
-                </ModalContent>
-                <ModalFooter>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button
-                        variant="primary"
-                        onClick={handleClose}
-                        ref={ctaRef}
-                    >
-                        Accept
-                    </Button>
-                </ModalFooter>
+            <Button onClick={toggleModal}>Open modal</Button>
+            <Modal
+                title="Title of the content goes here"
+                open={open}
+                onDismiss={toggleModal}
+                footer={
+                    <>
+                        <Button onClick={toggleModal}>Cancel</Button>
+                        <Button primary onClick={toggleModal}>
+                            Accept
+                        </Button>
+                    </>
+                }
+            >
+                <p>
+                    Content information goes here. Optional illustration on top.
+                    Can contain links.
+                </p>
+                <a href="#" onClick={(event) => event.preventDefault()}>
+                    Optional link to read more.
+                </a>
             </Modal>
         </>
     );
 };
 
 export const Overflowing = () => {
-    const [isOpen, setIsOpen] = React.useState(true);
-    const ctaRef = React.useRef(null);
-    const handleOpen = () => setIsOpen(true);
-
-    const handleClose = () => setIsOpen(false);
+    const [open, setOpen] = React.useState(true);
+    const toggleModal = () => setOpen(!open);
 
     return (
         <>
-            <Button onClick={handleOpen}>Open modal</Button>
-            <Modal isOpen={isOpen} onDismiss={handleClose}>
-                <ModalContent>
-                    <div className="u-bg-toothpaste rounded-8 pt-64 pb-32 mb-8" />
-                    <ModalHeading className="mt-16">
-                        Title of the content goes here
-                    </ModalHeading>
+            <Button onClick={toggleModal}>Open modal</Button>
+            <Modal
+                isOpen={open}
+                onDismiss={toggleModal}
+                title="Title of the content goes here"
+                footer={
+                    <>
+                        <Button onClick={toggleModal}>Cancel</Button>
+                        <Button primary onClick={toggleModal}>
+                            Accept
+                        </Button>
+                    </>
+                }
+            >
+                <div className="u-bg-toothpaste rounded-8 pt-64 pb-32 mb-8" />
 
-                    {Array.from(new Array(10), (val, index) => (
-                        <p key={index}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.
-                        </p>
-                    ))}
-                </ModalContent>
-                <ModalFooter>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button
-                        variant="primary"
-                        onClick={handleClose}
-                        ref={ctaRef}
-                    >
-                        Accept
-                    </Button>
-                </ModalFooter>
+                {new Array(10).map(_, (i) => (
+                    <p key={i}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                ))}
             </Modal>
         </>
     );
