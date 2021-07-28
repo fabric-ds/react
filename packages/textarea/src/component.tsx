43cd85f78aa14b745d081ffe7eb4050a2e0e5f1d
Benjamin Akar <benjaminakar2001@gmail.com>
2021-07-28 13:17:27 +0200

    a bit more refactoring

--- packages/textarea/src/component.tsx
+++ packages/textarea/src/component.tsx
@@ -7,65 +7,62 @@ import useTextAreaHeight from './useTextAreaHeight';
 /**
  * A textarea component that automatically resizes as content changes.
  */
-function TextArea(
-    {
-        className,
-        disabled,
-        error,
-        helpText,
-        id: providedId,
-        invalid,
-        label,
-        maximumRows,
-        minimumRows,
-        readOnly,
-        style,
-        value,
-        ...props
-    }: TextAreaProps,
-    forwardRef: Ref<HTMLTextAreaElement>,
-) {
-    const ref = useRef<HTMLTextAreaElement | null>(null);
+export const TextArea = forwardRef(
+    (
+        {
+            className,
+            disabled,
+            error,
+            helpText,
+            id: providedId,
+            invalid,
+            label,
+            maximumRows,
+            minimumRows,
+            readOnly,
+            style,
+            value,
+            ...props
+        }: TextAreaProps,
+        forwardRef: Ref<HTMLTextAreaElement>,
+    ) => {
+        const id = useId(providedId);
+        const ref = useRef<HTMLTextAreaElement | null>(null);
 
-    useTextAreaHeight({
-        ref,
-        value,
-        maximumRows,
-        minimumRows,
-    });
+        const helpId = helpText ? `${id}__hint` : undefined;
+        const isInvalid = invalid ?? error;
 
-    const id = useId(providedId);
-    const helpId = helpText ? `${id}__hint` : undefined;
+        useTextAreaHeight({
+            ref,
+            value,
+            maximumRows,
+            minimumRows,
+        });
 
-    const isInvalid = invalid ?? error;
-
-    const classes = {
-        'input--is-invalid': isInvalid,
-        'input--is-disabled': disabled,
-        'input--is-read-only': readOnly,
-    };
-
-    return (
-        <div
-            className={classNames('input mb-0', classes, className)}
-            style={style}
-        >
-            {label && <label htmlFor={id}>{label}</label>}
-            <textarea
-                {...props}
-                aria-describedby={helpId}
-                aria-errormessage={isInvalid && helpId ? helpId : undefined}
-                aria-invalid={isInvalid}
-                disabled={disabled}
-                id={id}
-                ref={forwardRef || ref}
-                readOnly={readOnly}
-                value={value}
-            />
-            {helpText && <div className="input__sub-text">{helpText}</div>}
-        </div>
-    );
-}
-
-const _TextArea = forwardRef(TextArea);
-export { _TextArea as TextArea };
+        return (
+            <div
+                className={classNames(className, {
+                    'input mb-0': true,
+                    'input--is-invalid': isInvalid,
+                    'input--is-disabled': disabled,
+                    'input--is-read-only': readOnly,
+                })}
+                style={style}
+            >
+                {label && <label htmlFor={id}>{label}</label>}
+                <textarea
+                    {...props}
+                    aria-describedby={helpId}
+                    aria-errormessage={isInvalid && helpId ? helpId : undefined}
+                    aria-invalid={isInvalid}
+                    disabled={disabled}
+                    id={id}
+                    ref={forwardRef || ref}
+                    readOnly={readOnly}
+                    value={value}
+                />
+                {helpText && <div className="input__sub-text">{helpText}</div>}
+            </div>
+        );
+    },
+);
