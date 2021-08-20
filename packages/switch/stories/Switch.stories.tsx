5f80bfb5ebe6e5cd724bdf5241625249c9c71366
tor-martin-storsletten <tor-martin.storsletten@finn.no>
2021-08-20 03:24:29 +0200

    chore(switch): Fix broken storybook examples

--- packages/switch/stories/Switch.stories.tsx
+++ packages/switch/stories/Switch.stories.tsx
@@ -7,21 +7,22 @@ export default metadata;
 export const DefaultDisabled = () => {
     const [value, setValue] = useState(false);
 
-    return <Switch onClick={(value) => setValue(value)} value={value} />;
+    return <Switch onClick={() => setValue(!value)} value={value} />;
 };
 
 export const DefaultEnabled = () => {
     const [value, setValue] = useState(true);
 
-    return <Switch onClick={(value) => setValue(value)} value={value} />;
+    return <Switch onClick={() => setValue(!value)} value={value} />;
 };
 
 export const CustomClickHandler = () => {
     const [value, setValue] = useState(false);
 
-    const handleClick = (value) => {
-        setValue(value);
-        alert('Your own custom click handler');
+    const handleClick = () => {
+        const newValue = !value;
+        setValue(newValue);
+        alert(`Custom click handler: Switch ${newValue ? 'enabled' : 'disabled'}.`);
     };
 
     return <Switch onClick={handleClick} value={value} />;
