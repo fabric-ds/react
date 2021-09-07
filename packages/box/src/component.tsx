3955cc7b17993d59d7bd9c9ba56a48778dd03ac7
Benjamin Akar <benjaminakar2001@gmail.com>
2021-09-07 13:44:57 +0200

    Add Clickable component to Box

--- packages/box/src/component.tsx
+++ packages/box/src/component.tsx
@@ -8,21 +8,6 @@ export function Box({ children, as = 'div', ...props }: BoxProps) {
         as,
         {
             ...(props as Omit<BoxProps, 'children'> as {}),
-            tabIndex: props.clickable ? 0 : undefined,
-            onKeyDown: props.clickable
-                ? (event) => {
-                      // Manually mapping Enter and Space keydown events to the click event (if there is one).
-                      // The browser doesn't do this automatically unless the element is a button or an a-element.
-                      // The Box element can't be a button or link in case someone puts an interactive element inside the box, which would result in invalid HTML and severe a11y issues.
-                      if (
-                          typeof props.onClick === 'function' &&
-                          (event.key === 'Enter' || event.key === ' ')
-                      ) {
-                          props.onClick(event);
-                          return false;
-                      }
-                  }
-                : undefined,
             className: classNames(
                 box.box,
                 {
@@ -39,13 +24,6 @@ export function Box({ children, as = 'div', ...props }: BoxProps) {
                 props.className,
             ),
         },
-        props.clickable ? (
-            <>
-                <div>{children}</div>
-                <span role="button" aria-label="Les mer"></span>
-            </>
-        ) : (
-            children
-        ),
+        children,
     );
 }
