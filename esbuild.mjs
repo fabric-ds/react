d653c454ebde67dd2dbae24aeb37d8f421d47b8b
Richard <digitalsadhu@gmail.com>
2021-10-05 17:12:11 +0200

    fix: use esbuild for eik, tsc for npm

---
+++ esbuild.mjs
@@ -0,0 +1,15 @@
+import { plugin } from '@eik/esbuild-plugin';
+import esbuild from 'esbuild';
+
+esbuild
+  .build({
+    entryPoints: ['index.ts'],
+    bundle: true,
+    outdir: 'eik',
+    format: 'esm',
+    sourcemap: true,
+    plugins: [plugin()],
+    target: 'es2017',
+    minify: true,
+  })
+  .catch(() => process.exit(1));
