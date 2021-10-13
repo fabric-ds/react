3f232daee406435acdc48675603a8c7efa034980
Richard <digitalsadhu@gmail.com>
2021-10-13 16:26:41 +0200

    Revert "refactor: externalise all dependencies"
    This reverts commit 45fc43af6000085af237ef798a0ef9e1f26eb824.

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,17 +1,7 @@
-import fs from 'fs';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
 import * as eik from '@eik/esbuild-plugin';
 import esbuild from 'esbuild';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 await eik.load();
-
-const pkg = JSON.parse(fs.readFileSync(join(__dirname, 'package.json')));
-const excludes = Object.keys(pkg.peerDependencies).concat(
-  Object.keys(pkg.dependencies),
-);
 
 await esbuild.build({
   plugins: [eik.plugin()],
@@ -22,15 +12,4 @@ await esbuild.build({
   sourcemap: true,
   target: 'es2017',
   minify: true,
-});
-
-await esbuild.build({
-  entryPoints: ['packages/index.ts'],
-  bundle: true,
-  outdir: 'dist/npm',
-  format: 'esm',
-  sourcemap: true,
-  target: 'es2021',
-  minify: true,
-  external: excludes,
 });
