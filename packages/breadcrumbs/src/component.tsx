257047e8723a43e5e968fadd7adca5a99e2400d4
Benjamin Akar <benjaminakar2001@gmail.com>
2021-12-08 12:59:47 +0100

    remove unused function

--- packages/breadcrumbs/src/component.tsx
+++ packages/breadcrumbs/src/component.tsx
@@ -6,7 +6,7 @@ export const Breadcrumbs = (props: BreadcrumbsProps) => {
   const { children, className, ...rest } = props;
   const ariaLabel = props['aria-label'] || 'Her er du';
 
-  const flatChildren = recurse(React.Children.toArray(children), []);
+  const flatChildren = recurse(children, []);
 
   function recurse(obj, array) {
     for (let i = 0; i < obj.length; i++) {
