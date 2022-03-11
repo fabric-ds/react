ad39ec67ef234154fcd52fda7079f519ca734aae
Benjamin Akar <benjaminakar2001@gmail.com>
2022-03-11 09:25:15 +0100

    fix: don't spread custom props onto underlying dom element on box component

--- packages/box/src/component.tsx
+++ packages/box/src/component.tsx
@@ -1,24 +1,34 @@
 import { classNames } from '@chbphone55/classnames';
 import { box } from '@fabric-ds/component-classes';
 import React from 'react';
-import type { BoxProps } from './props';
+import { BoxProps } from './props';
 
-export function Box({ children, as = 'div', ...props }: BoxProps) {
+export function Box(props: BoxProps) {
+  const {
+    children,
+    as = 'div',
+    bleed,
+    clickable,
+    neutral,
+    bordered,
+    info,
+    ...rest
+  } = props;
+
   return React.createElement(
     as,
     {
-      ...(props as Omit<BoxProps, 'children'> as {}),
+      ...(rest as Omit<BoxProps, 'children'> as {}),
       className: classNames(
         box.box,
         {
-          [box.bleed]: props.bleed,
-          [box.clickable]: props.clickable,
-          'bg-aqua-50': props.info,
-          'hover:bg-aqua-100 active:bg-aqua-200': props.info && props.clickable,
-          'bg-bluegray-100': props.neutral,
-          'hover:bg-bluegray-200 active:bg-bluegray-300':
-            props.neutral && props.clickable,
-          'border-2 border-bluegray-300': props.bordered,
+          [box.bleed]: bleed,
+          [box.clickable]: clickable,
+          'bg-aqua-50': info,
+          'hover:bg-aqua-100 active:bg-aqua-200': info && clickable,
+          'bg-bluegray-100': neutral,
+          'hover:bg-bluegray-200 active:bg-bluegray-300': neutral && clickable,
+          'border-2 border-bluegray-300': bordered,
         },
         props.className,
       ),
