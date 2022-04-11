a772bb3d292742b49bbb287da5b6189c3b65ed9c
Benjamin Akar <benjaminakar2001@gmail.com>
2022-04-11 14:43:53 +0200

    chore: add slack webhook token back to semantic release config and in action

--- release.config.cjs
+++ release.config.cjs
@@ -26,6 +26,7 @@ module.exports = {
         branchesConfig: [
           {
             pattern: 'main',
+            slackWebhook: 'SLACK_WEBHOOK',
             notifyOnSuccess: true,
             onSuccessTemplate: {
               text: '$package_name $npm_package_version is now available - $repo_url',
@@ -33,6 +34,7 @@ module.exports = {
           },
           {
             pattern: 'next',
+            slackWebhook: 'SLACK_WEBHOOK_PRIVATE',
             notifyOnSuccess: true,
             onSuccessTemplate: {
               text: '$package_name $npm_package_version (pre-release) is now available - $repo_url',
