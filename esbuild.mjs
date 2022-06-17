bc8f5041288270abf28496cc65640f37c75d348e
Richard Walker <digitalsadhu@gmail.com>
2022-06-17 13:27:41 +0200

    feat: support multiple versions of react on Eik

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,13 +1,28 @@
+import { ok } from 'node:assert';
 import * as eik from '@eik/esbuild-plugin';
 import esbuild from 'esbuild';
 
-await eik.load();
+// maps React versions to import map file versions.
+// Add more mappings here when new versions of React become available.
+const versions = new Map([
+  ['16', 'v1'],
+  ['17', 'v2'],
+  ['18', 'v3'],
+]);
+
+const version = process.argv[2];
+const reactVersions = Array.from(versions.keys());
+ok(reactVersions.includes(version), `Version argument is required. Must be one of: ${reactVersions.join(',')}. Eg. 'node esbuild.mjs 18'`);
+
+await eik.load({
+  urls: [`https://assets.finn.no/map/react/${versions.get(version)}`],
+});
 
 await esbuild.build({
   plugins: [eik.plugin()],
   entryPoints: ['packages/index.ts'],
   bundle: true,
-  outdir: 'dist/eik',
+  outfile: `dist/eik/fabric-react-${version}.js`,
   format: 'esm',
   sourcemap: true,
   target: 'es2017',
