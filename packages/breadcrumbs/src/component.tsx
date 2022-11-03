7d828fbe7ca5e88be1e88c0ac6411be0c9891bc8
BalbinaK <balbuhhha@gmail.com>
2022-11-03 11:13:22 +0100

    refactor: handle infinitly nested arrays

--- packages/breadcrumbs/src/component.tsx
+++ packages/breadcrumbs/src/component.tsx
@@ -7,8 +7,8 @@ export const Breadcrumbs = (props: BreadcrumbsProps) => {
   const { children, className, ...rest } = props;
   const ariaLabel = props['aria-label'] || 'Her er du';
 
-  // Handles array of nodes passed as children
-  const flattenedNodes = children.flat();
+  // Handles arrays of nodes passed as children
+  const flattenedChildren = children.flat(Infinity);
 
   return (
     <nav
@@ -18,7 +18,7 @@ export const Breadcrumbs = (props: BreadcrumbsProps) => {
     >
       <h2 className="sr-only">{ariaLabel}</h2>
       {interleave(
-        flattenedNodes,
+        flattenedChildren,
         <span aria-hidden className="select-none">
           /
         </span>,
